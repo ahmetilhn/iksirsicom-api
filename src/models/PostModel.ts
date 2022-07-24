6cc50c2919f14c7d2da0b17dfb961dc24db8d797
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-07-24 12:57:38 +0300

    PostModel improved

--- src/models/PostModel.ts
+++ src/models/PostModel.ts
@@ -1,49 +1,48 @@
 import { model, Schema } from "mongoose";
 import { IPost } from "../types/IPost";
 
-const postSchema = new Schema<IPost>({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  poster: {
-    type: String,
-    required: false,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  reading_time: {
-    type: Number || String,
-    required: false,
-  },
-  info: {
-    view: {
-      type: Object,
-      required: false,
+export default class PostModel {
+  public postSchema = new Schema<IPost>({
+    title: {
+      type: String,
+      required: true,
     },
-    like: {
-      type: Object,
-      required: false,
+    description: {
+      type: String,
+      required: true,
     },
-    author: { full_name: String, user_name: String, avatar: String },
-  },
-  tags: [
-    {
+    poster: {
       type: String,
       required: false,
     },
-  ],
-});
-
-export default class PostModel {
-  constructor(public postData: IPost) {
-    this.postData = postData;
-    model<IPost>("Post", postSchema);
+    content: {
+      type: String,
+      required: true,
+    },
+    reading_time: {
+      type: Number || String,
+      required: false,
+    },
+    info: {
+      view: {
+        type: Object,
+        required: false,
+      },
+      like: {
+        type: Object,
+        required: false,
+      },
+      author: { full_name: String, user_name: String, avatar: String },
+    },
+    tags: [
+      {
+        type: String,
+        required: false,
+      },
+    ],
+  });
+  public Model = model<IPost>("Post", this.postSchema);
+  public save(postData: IPost) {
+    new this.Model(postData).save();
   }
 }
