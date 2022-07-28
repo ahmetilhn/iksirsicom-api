a82bb0526c1c025d5af3641abde4af296516da67
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-07-29 00:29:28 +0300

    authors data added

---
+++ src/data/authors.js
@@ -0,0 +1,24 @@
+export default {
+  SAFA_GAYRET: {
+    full_name: "Safa Gayret",
+    social_links: {
+      medium: "https://safa.medium.com/",
+      linkedin: "https://www.linkedin.com/in/safagayret/",
+      website: "https://www.safagayret.com/",
+      github: "https://github.com/gayret",
+    },
+    avatar:
+      "https://miro.medium.com/fit/c/176/176/1*0cI-9VyAwPJhkfUO53Hi-Q.jpeg",
+  },
+  AHMET_ILHAN: {
+    full_name: "Ahmet İlhan",
+    social_links: {
+      medium: "https://safa.medium.com/",
+      linkedin: "https://www.linkedin.com/in/safagayret/",
+      website: "https://www.safagayret.com/",
+      github: "https://github.com/gayret",
+    },
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQF52Tcii0IDBw/profile-displayphoto-shrink_800_800/0/1645993304620?e=1664409600&v=beta&t=7CCmp4Gk6kfDm6DloqPoYDjB-vX2GQQQ6eMb0Verk7M",
+  },
+};
